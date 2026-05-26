409320fbda49c56ea63085611118cca7b0c6ac0a
Webb <mdw2685@eid.utexas.edu>
2026-05-26 13:05:05 -0500

    added new text project pages, broke apart css, format fixes

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import pandas as pd
 import json
 # import psycopg2
 
-app = Flask(__name__, static_url_path='', static_folder='./frontend/static', template_folder='./frontend/templates')
+app = Flask(__name__, static_url_path='', static_folder='./frontend/static',
+            template_folder='./frontend/templates')
+
 
 @app.route("/")
 def frontpage():
@@ -20,10 +22,10 @@ def slfly():
     df = pd.read_csv('./frontend/static/js/lanternfly/observations.csv')
     observation_dict = df.to_dict(orient='records')
     # Convert to geoJSON
-    geoJSON = {"type": "FeatureCollection", "features":[]}
+    geoJSON = {"type": "FeatureCollection", "features": []}
     for observation in observation_dict:
         geoJSON["features"].append({
-            "type":"Feature",
+            "type": "Feature",
             "geometry": {
                 "type": "Point",
                 "coordinates": [observation['longitude'], observation['latitude']]
@@ -35,9 +37,21 @@ def slfly():
                 "id": observation['id']
             }
         })
-    
+
     # Show lanternfly page
     return render_template("lanternfly.html", lanternflyData=geoJSON)
+
+
+@app.route("/current")
+def currentProjects():
+    # Show current projects page
+    return render_template("current.html")
+
+
+@app.route("/occurrence-records")
+def occurrenceRecords():
+    # Show occurrence records page
+    return render_template("occurrence_records.html")
 
 
 @app.route("/me")
@@ -52,37 +66,6 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/weatherblanket", methods=['GET', 'POST'])
-def wblanket():
-    today = date.today()
-    delta = timedelta(days = 365)
-    first_day = today - delta
-    
-    # Connect to NCEI API for 2022
-    site = "https://www.ncei.noaa.gov/access"
-    endpoint = "/services/data/v1?dataset=global-summary-of-the-day&stations=72254413958&" \
-               "startDate={}&endDate={}&dataTypes=MAX,MIN,PRCP&format=json".format(first_day, today)
-
-    response = get("{}{}".format(site, endpoint))
-    print(response)
-    weatherdata = response.json()
-    
-    # Intialize list for calculating max rain
-    rain_list = []
-    daynum = len(weatherdata)
-    
-    # Add each rain value to list
-    for day in weatherdata:
-        rain_list.append(day['PRCP'])
-      
-    # Sort list with max rain at top and take value  
-    rain_list.sort(reverse=True)
-    max_rain = rain_list[0]
-        
-    #Show weatherblanket page
-    return render_template("weatherblanket.html", weatherdata=weatherdata, daynum=daynum, max_rain=max_rain)
-
-
 @app.route("/discography", methods=['GET', 'POST'])
 def discography():
     # Open select_discography.csv and read into variable
@@ -91,7 +74,7 @@ def discography():
         disc_count = len(discography_data)
 
     # Show discography page
-    return render_template("discography.html", discographyData = discography_data, discCount = disc_count)
+    return render_template("discography.html", discographyData=discography_data, discCount=disc_count)
 
 
 @app.route("/projects")
@@ -112,7 +95,7 @@ def touchgrass():
 
 # @app.route("/entomology-entries")
 # def entomology_entries():
-#     # Get CSV for 
+#     # Get CSV for
 
 # Presentation for weatherblanket versions
 # @app.route("/presentation")
--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
 set FLASK_APP=app.py
-set FLASK_DEBUG=1
-flask run
+python -m flask run --debug
